0d045935c29f5e2bf3b0b081e54773646811901b
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-07-09 00:20:04 +0400

    Add project files

---
+++ CSharp_Fundamentals_Part1_Tasks(1)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_Fundamentals_Part1_Tasks_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
