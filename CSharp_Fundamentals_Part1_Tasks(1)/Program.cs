a71da9c3afe802ff10c4edd2e0876eb7d23bf88e
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-07-09 01:22:09 +0400

    implement problem 9 - Day Name Finder

--- CSharp_Fundamentals_Part1_Tasks(1)/Program.cs
+++ CSharp_Fundamentals_Part1_Tasks(1)/Program.cs
@@ -173,6 +173,43 @@
 
 
             ///////////////////////////////////////////////////////
+            
+            //Task9 - Day Name Finder
+
+            Console.WriteLine("Enter a number from 1 to 7 representing a day of the week: ");
+            int DayNumber = int.Parse(Console.ReadLine());
+
+            switch (DayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thuersday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+            }
+            Console.WriteLine("_____________________________________");
+
+
+            ///////////////////////////////////////////////////////
 
 
 
