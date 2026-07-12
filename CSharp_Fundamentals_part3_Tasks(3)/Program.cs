5f58b5e9aade5edadf94266b669cafb9c53f8b49
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-07-12 10:25:43 +0400

    Implement problem 1 - Absolute Difference

--- CSharp_Fundamentals_part3_Tasks(3)/Program.cs
+++ CSharp_Fundamentals_part3_Tasks(3)/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Task 1 - Absolute Difference
+
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int difference = Math.Abs(num2 - num1);
+
+            Console.WriteLine("Positive difference: " + difference);
+            Console.WriteLine("_____________________________________");
+
+
+            ///////////////////////////////////////////////////////
+
         }
     }
 }
