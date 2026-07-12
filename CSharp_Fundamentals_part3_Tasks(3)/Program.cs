d87d630df4b54047d800ef05ca7db46241c0072c
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-07-12 10:28:19 +0400

    Implement 2 - Power & Root Explorer

--- CSharp_Fundamentals_part3_Tasks(3)/Program.cs
+++ CSharp_Fundamentals_part3_Tasks(3)/Program.cs
@@ -20,6 +20,22 @@
 
             ///////////////////////////////////////////////////////
 
+
+            //Task2 - Power & Root Explorer
+
+            Console.Write("Enter a number: ");
+            double number = double.Parse(Console.ReadLine());
+
+            double square = Math.Pow(number, 2);
+            double squareRoot = Math.Sqrt(number);
+
+            Console.WriteLine("Square: " + square);
+            Console.WriteLine("Square Root: " + squareRoot);
+            Console.WriteLine("_____________________________________");
+
+
+            ///////////////////////////////////////////////////////
+
         }
     }
 }
