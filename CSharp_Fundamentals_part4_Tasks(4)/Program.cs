b2d93a68c4d15ffa206736d7989f047c6ce5f3e1
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-07-13 18:29:10 +0400

    Implement problem 2 - Square Number Function

--- CSharp_Fundamentals_part4_Tasks(4)/Program.cs
+++ CSharp_Fundamentals_part4_Tasks(4)/Program.cs
@@ -12,13 +12,29 @@
             Console.WriteLine("Welcome, " + name + "!");
         }
 
-        /////////////////////    Main   ///////////////////////////
+        // Task 2 – Square Number Function
+        static int Square(int number)
+        {
+            return number * number;
+        }
+
+        /////////////////////    Main     ///////////////////////////
         static void Main(string[] args)
         {
             // Task1 
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
             PrintWelcome(name);
+
+            Console.WriteLine();
+
+            // Task 2 
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int square = Square(number);
+
+            Console.WriteLine("Square = " + square);
 
             Console.WriteLine();
 
