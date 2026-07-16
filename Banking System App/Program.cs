190ca45bcf61101c747aab546299cf191eb22201
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-07-16 09:28:54 +0400

    Add project files

---
+++ Banking System App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Banking_System_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
