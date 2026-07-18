5b8e03e4cd7e117979927e6286e01b5f515ed6f2
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-07-19 03:28:10 +0400

    Implement class BankAccount

--- OOP_Part1/Program.cs
+++ OOP_Part1/Program.cs
@@ -1,10 +1,53 @@
 ﻿namespace OOP_Part1
 {
+    class BankAccount
+    {
+        public int AccountNumber;
+        public string HolderName;
+        public double Balance;
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+            SendEmail();
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                SendEmail();
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance.");
+            }
+        }
+
+        public double CheckBalance()
+        {
+            PrintInformation();
+            return Balance;
+        }
+
+        private void PrintInformation()
+        {
+            Console.WriteLine("Holder Name: " + HolderName);
+            Console.WriteLine("Balance: " + Balance);
+        }
+
+        private void SendEmail()
+        {
+            Console.WriteLine("Email notification sent.");
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
