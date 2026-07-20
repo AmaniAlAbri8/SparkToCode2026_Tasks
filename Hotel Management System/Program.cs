b603bf855d37a90a9f890dfd3a70e0f275343d72
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-07-20 05:39:33 +0400

    Implement room class

---
+++ Hotel Management System/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hotel_Management_System
+{
+    // Room class
+    public class Room
+    {
+        
+        public int roomNumber;
+        public string roomType;
+        public double pricePerNight;
+        public bool isAvailable;
+
+
+        // Constructor
+        public Room(int number, string type, double price)
+        {
+            roomNumber = number;
+            roomType = type;
+            pricePerNight = price;
+            isAvailable = true;
+        }
+
+
+        // Method to display room information
+        public void displayRoom()
+        {
+            string status;
+
+            if (isAvailable == true)
+            {
+                status = "Available";
+            }
+            else
+            {
+                status = "Booked";
+            }
+
+
+            Console.WriteLine(
+                "Room Number: " + roomNumber +
+                " | Type: " + roomType +
+                " | Price: OMR " + pricePerNight.ToString("0.00") +
+                " | Status: " + status
+            );
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+        }
+    }
+}
